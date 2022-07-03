30620a5751a65ed29c773b776e9ec5700ec5cdc6
Andrii <64665760+hiddenns@users.noreply.github.com>
2022-07-03 13:47:41 +0300

    change login & reg style

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,12 @@
+alert("we");
+$(".user").focusin(function(){
+  $(".inputUserIcon").css("color", "#e74c3c");
+}).focusout(function(){
+  $(".inputUserIcon").css("color", "white");
+});
+
+$(".pass").focusin(function(){
+  $(".inputPassIcon").css("color", "#e74c3c");
+}).focusout(function(){
+  $(".inputPassIcon").css("color", "white");
+});
